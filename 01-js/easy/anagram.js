02c35e1a01629c050874c405f697cbb3353dfd57
amjuz <137517288+amjuz@users.noreply.github.com>
2023-12-29 11:13:05 +0530

    updating few assignments

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,11 +1,25 @@
-/*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
-  What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
-*/
-
+let p = Promise()
 function isAnagram(str1, str2) {
 
-}
+  let sortedArray1 = str1.toLowerCase().split('').sort();
+  let sortedArray2 = str2.toLowerCase().split('').sort();
+  let temp = true;
 
+  if (sortedArray1.length === sortedArray2.length) {
+
+    for (let i = 0; i < sortedArray1.length; i++) {
+
+      if (sortedArray1[i] !== sortedArray2[i]) {
+        temp = false;
+        break;
+
+      }
+
+    }
+
+  } else {
+    temp = false;
+  }
+  return temp;
+}
 module.exports = isAnagram;
--- week-2/01-async-js/easy/2-counter.md
+++ week-2/01-async-js/easy/2-counter.md
@@ -2,7 +2,20 @@
 
 Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
 
+function printSeconds(){
+  
+  console.clear();
+  console.log(++count)
+  
+  setTimeout(()=>{
+    printSeconds();
+  },1000)
+  
+}
 
+let count = 0;
+printSeconds();
+  
 
 
 
